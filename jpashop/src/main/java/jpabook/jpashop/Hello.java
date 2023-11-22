d31d2420f1c18c6c049576a6eb6f8e6f6c7806e1
0Chord <seaweed.0chord@gmail.com>
2023-11-22 17:21:04 +0900

    feat : 추가 프로젝트 환경설정

--- jpashop/src/main/java/jpabook/jpashop/Hello.java
+++ jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -1,2 +1,18 @@
-package jpabook.jpashop;public class Hello {
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+    private String data;
+
+    public Hello(String data) {
+        this.data = data;
+    }
+
+    public static Hello make(String data) {
+        return new Hello(data);
+    }
 }
