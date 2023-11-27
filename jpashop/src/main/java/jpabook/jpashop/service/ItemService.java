2776cac9ecbfc159af9027f3e5d2fc80c2b4f64d
0Chord <seaweed.0chord@gmail.com>
2023-11-27 23:38:10 +0900

    feat : 추가 Item domain view

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import java.util.List;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+
+
     }
 
     public List<Item> findItems() {
